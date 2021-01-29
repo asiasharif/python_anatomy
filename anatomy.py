f5245a2a652619d4dd14bb6fbed8381f5a967e6c
Asia <asiasharif12@icloud.com>
2021-01-29 15:37:45 +0000

    Added anatomy condition argument

---
+++ anatomy.py
@@ -0,0 +1,13 @@
+import sys
+
+argc = len(sys.argv)
+
+if argc > 1:
+    print('Too many args')
+else:
+    where = 'World'
+    print("Hello", where)
+
+print('Goodbye from' +
+      sys.argv[0])
+
